abf0e37b97d274249520828a75dbae3f21c9be80
Jacory <jacory.jin@gmail.com>
2025-12-10 10:28:48 +0800

    初始化项目

---
+++ src/main/java/io/github/jacorycyjin/smartlibrary/backend/SmartLibraryBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.jacorycyjin.smartlibrary.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLibraryBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartLibraryBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/jacorycyjin/smartlibrary/backend/SmartLibraryBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jacorycyjin.smartlibrary.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLibraryBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
